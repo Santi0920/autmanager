98653918cdf839a7ece683625354834b122b24d7
Santi0920 <santicashe0920@gmail.com>
2024-04-15 13:56:27 -0500

    jefatura

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -81,7 +81,7 @@ class SessionsController extends Controller
             return redirect()->to('/validar');
 
 
-        } else if ($user->rol == 'Gerencia') {
+        } else if ($user->rol == 'Jefatura') {
             $usuarioActual = Auth::user();
             $nombre = $usuarioActual->name;
             $rol = $usuarioActual->rol;
