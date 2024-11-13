777f7d7042d924097f6d7ed5ead89143f8b5359a
Santi0920 <santicashe0920@gmail.com>
2024-11-12 16:21:42 -0800

    implementacion del cache para coordinaciones

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'disable.cache' => \App\Http\Middleware\DisableCache::class,
     ];
     /*
         protected $middlewareAliases = [
