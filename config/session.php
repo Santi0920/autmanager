ebb1146c834f1085f2841c30450546700ed38c25
Santi0920 <santicashe0920@gmail.com>
2025-08-19 08:49:58 -0500

    session actualizado

--- config/session.php
+++ config/session.php
@@ -34,7 +34,7 @@ return [
     // 'lifetime' => env('SESSION_LIFETIME', 10),
     'lifetime' => env('SESSION_LIFETIME', 600),
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
     /*
     |--------------------------------------------------------------------------
     | Session Encryption
