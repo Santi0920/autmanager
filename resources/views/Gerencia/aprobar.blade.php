c9a271745b49a736d24ed0c3aad104843686efbc
Santi0920 <santicashe0920@gmail.com>
2025-08-20 17:28:41 -0500

    gerencia

--- resources/views/Gerencia/aprobar.blade.php
+++ resources/views/Gerencia/aprobar.blade.php
@@ -442,7 +442,7 @@
                                                             <span class="fs-5">${row.Concepto} - @include('layouts.optionvercodigo')</span>
                                                         </div>
                                                         <div class="col-sm-6 col-md-3 col-lg-3 d-flex align-items-center justify-content-center border p-3">
-                                                            ${row.CodigoAutorizacion == "11K" ? `<span class="fs-5 fw-bold mb-0">${row.Convencion}</span>` : ``}
+                                                            ${row.CodigoAutorizacion == "41" ? `<span class="fs-5 fw-bold mb-0">${row.Convencion}</span>` : ``}
                                                         </div>
                                                     </div>
 
